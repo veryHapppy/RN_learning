26becf6662a74eb6c89a701503b4b6b5485c5e33
hwanhee <jeehh0331@gmail.com>
2026-02-09 14:37:32 +0900

    init

---
+++ chattingApp/src/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "react-native";
+import { ThemeProvider } from 'styled-components/native';
+import { theme } from "./theme";
+
+const App = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <StatusBar barStyle="dark-content" />
+        </ThemeProvider>
+    );
+};
+
+export default App
---
+++ chattingApp/src/theme.ts
@@ -0,0 +1,13 @@
+const colors = {
+    white: '#ffffff',
+    black: '#000000',
+    gray_0: '#d5d5d5',
+    gray_1: '#a6a6a6',
+    red: '#e84118',
+    blue: '#3679fe',
+};
+
+export const theme = {
+    background: colors.white,
+    text: colors.black,
+};
